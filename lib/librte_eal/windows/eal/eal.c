4cb0023fd3aa420e2e28a838d5bb6d5e24534e1a
Anand Rawat <anand.rawat@intel.com>
2019-03-05 20:16:29 -0800

    eal: eal stub to add windows support
    Added initial stub source files for windows support and meson
    changes to build them.
    
    Signed-off-by: Anand Rawat <anand.rawat@intel.com>
    Signed-off-by: Pallavi Kadam <pallavi.kadam@intel.com>
    Reviewed-by: Jeff Shaw <jeffrey.b.shaw@intel.com>
    Reviewed-by: Ranjit Menon <ranjit.menon@intel.com>
    Signed-off-by: 0-day Robot <robot_email>

---
+++ lib/librte_eal/windows/eal/eal.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2019 Intel Corporation
+ */
+
+#include "rte_common.h"
+
+int
+rte_eal_init(int argc __rte_unused, char **argv __rte_unused)
+{
+	return 0;
+}
---
+++ lib/librte_eal/windows/eal/eal_debug.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2019 Intel Corporation
+ */
+
+#include "rte_common.h"
+
+void
+__rte_panic(const char *funcname __rte_unused,
+		const char *format __rte_unused, ...)
+{
+	abort();
+}
